997df94992660d97cf0e5769b5a086e1f9a9dd69
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-06-17 10:38:04 +0300

    Updates

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import MenuBar from "./MenuBar";
+import { FaBook } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <div className="bg-[#051B39] ">
       <div className="flex justify-between max-w-7xl m-auto p-6 items-center">
         <div>
-          <p className="text-white">My Notes</p>
+          <p className="text-white text-2xl">
+            <FaBook />
+          </p>
         </div>
         <div className="flex">
-          <div >
+          <div>
             <MenuBar />
           </div>
         </div>
